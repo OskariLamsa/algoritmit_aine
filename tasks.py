16d2bd56c75ec7a14974528175afab56588b6389
OskariLamsa <oskarilm50@gmail.com>
2025-11-21 23:22:28 +0000

    fix

--- tasks.py
+++ tasks.py
@@ -5,6 +5,5 @@ import sys
 @task
 def main(c):
     #c.run("poetry run python main.py")
-    def main(c):
-        cmd = ["poetry", "run", "python", "main.py"]
-        subprocess.run(cmd, check=False)
+    cmd = ["poetry", "run", "python", "main.py"]
+    subprocess.run(cmd, check=False)
